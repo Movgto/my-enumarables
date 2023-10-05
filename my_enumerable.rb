d7cda6b1ed35ba590c4394334cc3f1371fe7660a
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-10-05 23:57:45 +0100

    fix error related to linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -3,10 +3,12 @@ module MyEnumerable
     each { |element| return false unless yield element }
     true
   end
+
   def any?
     each { |element| return true if yield element }
     false
   end
+
   def filter
     result = []
     each { |element| result << element if yield element }
