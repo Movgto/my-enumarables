42280946382f5f6c6c8232560dd0be52aef5c39e
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-10-06 12:19:31 +0100

    Implemented custom enumerable methods and create a standalone custom each method in MyList class

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,17 +1,28 @@
 module MyEnumerable
-  def all?
-    each { |element| return false unless yield element }
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    counter = 0
+    while counter < size
+      yield(self[counter])
+      counter += 1
+    end
+    self
+  end
+
+  def my_all?
+    my_each { |element| return false unless yield(element) }
     true
   end
 
-  def any?
-    each { |element| return true if yield element }
+  def my_any?
+    my_each { |element| return true if yield(element) }
     false
   end
 
-  def filter
+  def my_filter
     result = []
-    each { |element| result << element if yield element }
+    my_each { |element| result << element if yield(element) }
     result
   end
 end
--- my_list.rb
+++ my_list.rb
@@ -2,18 +2,24 @@ require_relative 'my_enumerable'
 
 class MyList
   include MyEnumerable
+
   def initialize(*list)
     @list = list
   end
 
-  def each(&block)
-    @list.each(&block)
+  def [](index)
+    @list[index]
+  end
+
+  def size
+    @list.size
   end
 end
 
+
 list = MyList.new(1, 2, 3, 4)
-puts(list.all? { |e| e < 5 })
-puts(list.all? { |e| e > 5 })
-puts(list.any? { |e| e == 2 })
-puts(list.any? { |e| e == 5 })
-p(list.filter(&:even?))
+puts(list.my_all? { |e| e < 5 })
+puts(list.my_all? { |e| e > 5 })
+puts(list.my_any? { |e| e == 2 })
+puts(list.my_any? { |e| e == 5 })
+p(list.my_filter(&:even?))
